f33cfa848fe081903c7d8edc68d713fbfc92a58b
Minatricks <Dimanoit@gmail.com>
2019-11-11 23:34:37 +0200

    chore: add base configuration of frontend

--- ChatUI/src/app/app.module.ts
+++ ChatUI/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/components/user/user.component';
+import { SingUpComponent } from './user/components/sing-up/sing-up.component';
+import { SingInComponent } from './user/components/sing-in/sing-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    SingUpComponent,
+    SingInComponent
   ],
   imports: [
     BrowserModule,
