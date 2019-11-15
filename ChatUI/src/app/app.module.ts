92d1a10194cff7ebf2f2bb59fb2e10503693be8c
Minatricks <Dimanoit@gmail.com>
2019-11-15 15:44:05 +0200

    add: base message page

--- ChatUI/src/app/app.module.ts
+++ ChatUI/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { UserComponent } from './user/components/user/user.component';
 import { SingUpComponent } from './user/components/sing-up/sing-up.component';
 import { SingInComponent } from './user/components/sing-in/sing-in.component';
 import { MessageComponent } from './messages/components/message/message.component';
+import { ContactsComponent } from './messages/components/contacts/contacts.component';
+import { ContactComponent } from './messages/components/contact/contact.component';
+import { MessageViewComponent } from './messages/components/message-view/message-view.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import { MessageComponent } from './messages/components/message/message.componen
     UserComponent,
     SingUpComponent,
     SingInComponent,
-    MessageComponent
+    MessageComponent,
+    ContactsComponent,
+    ContactComponent,
+    MessageViewComponent
   ],
   imports: [
     BrowserModule,
